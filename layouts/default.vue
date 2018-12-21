86206e979ca1d0a34ef03d174b48a0da4bcc94a7
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-12-21 11:39:05 +0100

    Adjusted the toolbar.

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,27 +20,11 @@
 				</v-list-tile>
 			</v-list>
 		</v-navigation-drawer>
-		<v-toolbar :clipped-left="clipped"
-		 fixed
+		<v-toolbar fixed
 		 app>
-			<v-toolbar-side-icon @click="drawer = !drawer" />
-			<v-btn icon
-			 @click.stop="miniVariant = !miniVariant">
-				<v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-			</v-btn>
-			<v-btn icon
-			 @click.stop="clipped = !clipped">
-				<v-icon>web</v-icon>
-			</v-btn>
-			<v-btn icon
-			 @click.stop="fixed = !fixed">
-				<v-icon>remove</v-icon>
-			</v-btn>
-			<v-toolbar-title v-text="title" />
-			<v-btn icon
-			 @click.stop="rightDrawer = !rightDrawer">
-				<v-icon>menu</v-icon>
-			</v-btn>
+			<v-toolbar-title>
+				BŚP 2019
+			</v-toolbar-title>
 		</v-toolbar>
 		<v-content :style="{backgroundImage:imageURL }"
 		 id='main-background-window'>
