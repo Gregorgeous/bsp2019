eeebf43912bc5282f050a7e2d95d50f6d347ea2e
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-12-21 10:49:38 +0100

    (minor) formatted the code to my preferences. (Configured my prettier formatter differently to the Nuxt's standard)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,129 +1,116 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon" />
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content :style="{backgroundImage:imageURL }" id='main-background-window'>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+	<v-app dark>
+		<v-navigation-drawer :mini-variant="miniVariant"
+		 :clipped="clipped"
+		 v-model="drawer"
+		 fixed
+		 app>
+			<v-list>
+				<v-list-tile v-for="(item, i) in items"
+				 :to="item.to"
+				 :key="i"
+				 router
+				 exact>
+					<v-list-tile-action>
+						<v-icon v-html="item.icon" />
+					</v-list-tile-action>
+					<v-list-tile-content>
+						<v-list-tile-title v-text="item.title" />
+					</v-list-tile-content>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+		<v-toolbar :clipped-left="clipped"
+		 fixed
+		 app>
+			<v-toolbar-side-icon @click="drawer = !drawer" />
+			<v-btn icon
+			 @click.stop="miniVariant = !miniVariant">
+				<v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
+			</v-btn>
+			<v-btn icon
+			 @click.stop="clipped = !clipped">
+				<v-icon>web</v-icon>
+			</v-btn>
+			<v-btn icon
+			 @click.stop="fixed = !fixed">
+				<v-icon>remove</v-icon>
+			</v-btn>
+			<v-toolbar-title v-text="title" />
+			<v-btn icon
+			 @click.stop="rightDrawer = !rightDrawer">
+				<v-icon>menu</v-icon>
+			</v-btn>
+		</v-toolbar>
+		<v-content :style="{backgroundImage:imageURL }"
+		 id='main-background-window'>
+			<v-container>
+				<nuxt />
+			</v-container>
+		</v-content>
+		<v-navigation-drawer :right="right"
+		 v-model="rightDrawer"
+		 temporary
+		 fixed>
+			<v-list>
+				<v-list-tile @click.native="right = !right">
+					<v-list-tile-action>
+						<v-icon light>compare_arrows</v-icon>
+					</v-list-tile-action>
+					<v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+				</v-list-tile>
+			</v-list>
+		</v-navigation-drawer>
+		<v-footer :fixed="fixed"
+		 app>
+			<span>&copy; 2017</span>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        imageURL: null,
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
-    },
-    methods:{
-      randomBackgroundImage(){
-        let num = Math.floor(Math.random() * 10) + 1;
-        let backgroundGradient = 'linear-gradient(45deg, rgba(201, 101, 113, 0.831) 0%, rgba(93, 173, 207, 0.51) 81%),'
-        let randomImageURL = 'url('+ require(`@/assets/images/background-${num}.jpg`) + ')' 
-        this.imageURL = backgroundGradient + randomImageURL;
-      }
-    },
-    computed:{
-      
-    },
-    mounted(){
-      this.randomBackgroundImage()
+export default {
+  data() {
+    return {
+      imageURL: null,
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
     }
+  },
+  methods: {
+    randomBackgroundImage() {
+      let num = Math.floor(Math.random() * 10) + 1
+      let backgroundGradient =
+        'linear-gradient(45deg, rgba(201, 101, 113, 0.831) 0%, rgba(93, 173, 207, 0.51) 81%),'
+      let randomImageURL =
+        'url(' + require(`@/assets/images/background-${num}.jpg`) + ')'
+      this.imageURL = backgroundGradient + randomImageURL
+    }
+  },
+  computed: {},
+  mounted() {
+    this.randomBackgroundImage()
   }
+}
 </script>
 
-<style scoped>
-#main-background-window{
+<style lang="scss" scoped>
+#main-background-window {
   background-size: cover;
-  background-image: linear-gradient(45deg, rgba(201, 101, 113, 0.831) 0%, rgba(93, 173, 207, 0.51) 81%);
+  background-image: linear-gradient(
+    45deg,
+    rgba(201, 101, 113, 0.831) 0%,
+    rgba(93, 173, 207, 0.51) 81%
+  );
   background-position: center;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,26 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center>
-    <v-flex
-      xs12
-      sm8
-      md6>
-      <v-card>
-        <v-card-title class="headline ">BŚP 2019</v-card-title>
-        <v-card-text>
-          <h1 class='title'>Zaloguj się aby zacząć zabawę !</h1>
-          <hr class="my-3">
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer/>
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire">Kontynuuj</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+	<v-layout column
+	 justify-center
+	 align-center>
+		<v-flex xs12
+		 sm8
+		 md6>
+			<v-card>
+				<v-card-title class="headline ">BŚP 2019</v-card-title>
+				<v-card-text>
+					<h1 class='title'>Zaloguj się aby zacząć zabawę !</h1>
+					<hr class="my-3">
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer />
+					<v-btn color="primary"
+					 flat
+					 nuxt
+					 to="/inspire">Kontynuuj</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-flex>
+	</v-layout>
 </template>
 
 <script>
