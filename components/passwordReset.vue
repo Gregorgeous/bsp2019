b459d203c5bbf295fb62312b59c7eb87c516a542
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-12-22 23:23:48 +0100

    Created a code-draft of 'passwordReset' component to be included on both Register and Login forms.

---
+++ components/passwordReset.vue
@@ -0,0 +1,14 @@
+<template>
+	<!-- TODO: IMPLEMENT ! -->
+	<small>Zapomniane hasło?
+		<nuxt-link to="/">Kliknij tutaj!
+		</nuxt-link>
+	</small>
+</template>
+
+
+<style lang="scss" scoped>
+small {
+  color: #636363;
+}
+</style>
