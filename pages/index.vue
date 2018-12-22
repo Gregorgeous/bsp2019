83300674e3c2bb4cc7468111304c3cbf3de14f00
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-12-22 23:52:03 +0100

    Changed the markup to display game-participants' (dummy) data and use the previously-made small styling.

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,31 @@
 	 align-center>
 		<v-flex xs12>
 			<v-card>
-				<v-card-title class="headline ">BŚP 2019</v-card-title>
+				<v-card-title class="headline segment">
+					<h1 class='title'>Patrol:</h1>
+					<span>{{patrolName}} </span>
+				</v-card-title>
+				<v-divider></v-divider>
 				<v-card-text>
-					<h1 class='title'>Zaloguj się aby zacząć zabawę !</h1>
-					<hr class="my-3">
+					<div class='segment mb-1'>
+						<h2 class='subheading'>Metodyka:</h2>
+						<!-- TODO: Add the official zhp's metodyka logos support for each level-->
+						<h3>Harcerze starsi</h3>
+						<v-icon>supervisor_account</v-icon>
+					</div>
+					<v-divider class='mb-2'></v-divider>
+					<div class='segment'>
+						<h2 class='subheading'>Liczba pkt.:</h2>
+						<h3>{{score}}</h3>
+					</div>
+					<div class='segment'>
+						<h2 class='subheading'>Zrobionych zadań:</h2>
+						<h3>{{questsFinished}}</h3>
+					</div>
+					<div class='segment'>
+						<h2 class='subheading'>Pozostało zadań:</h2>
+						<h3>{{questsLeft}}</h3>
+					</div>
 				</v-card-text>
 			</v-card>
 		</v-flex>
