ba073a4cedecf6e1586c1d5428eef5b4d71c3675
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-12-22 23:31:19 +0100

    Created a page 'register' where I included the newly-created registerForm component.

---
+++ pages/register.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-layout column
+	 justify-center
+	 align-center>
+		<v-flex xs12
+		 sm8
+		 md6>
+			<registerForm></registerForm>
+		</v-flex>
+	</v-layout>
+</template>
+
+<script>
+import registerForm from '@/components/registerForm'
+export default {
+  components: {
+    registerForm
+  }
+}
+</script>
