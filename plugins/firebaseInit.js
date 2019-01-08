816d6455b76c907b37715e901ecae71ab281de75
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2019-01-08 23:58:00 +0000

    Added important code comment regarding FirebaseConfig.js for any potential repo pulls.

--- plugins/firebaseInit.js
+++ plugins/firebaseInit.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+// IMPORTANT: For any github pullers: set up your own firebase instance, and store it in  "Name_for_your_FBConfig.js" and import it just as I do below (modify the path for your own file)
 import fbConfig from '@/firebaseConfig.js'
 
 // IDEA: this 'if' prevents an occasional "Firebase application already initialized" error
